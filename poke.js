8d9f174d7fee674042a2eb3f7933ee5b13eda5c3
nikvijay07 <nikvijay07@gmail.com>
2023-10-04 21:00:35 -0400

    Formatiting for types list

--- poke.js
+++ poke.js
@@ -1,14 +1,36 @@
-let index = 1;
+let index = 20;
 
 let BASE_URL = `https://pokeapi.co/api/v2/pokemon/${index}/`;
 
 let data;
+
+const typeColor = {};
+typeColor["Normal"] = 'A8A77A';
+typeColor["Fire"] = "EE8130";
+typeColor["Water"] = "6390F0";
+typeColor["Electric"] = "F7D02C";
+typeColor["Grass"] = "7AC74C";
+typeColor["Ice"] = "96D9D6";
+typeColor["Fighting"] = "C22E28";
+typeColor["Poison"] = "A33EA1";
+typeColor["Ground"] = "E2BF65";
+typeColor["Flying"] = "A98FF3";
+typeColor["Psychic"] = "F95587";
+typeColor["Bug"] = "A6B91A";
+typeColor["Rock"] = "B6A136";
+typeColor["Ghost"] = "735797";
+typeColor["Dragon"] = "6F35FC";
+typeColor["Dark"] = "705746";
+typeColor["Steel"] = "B7B7CE";
+typeColor["Fairy"] = "D685AD";
 
 
 async function fetchData() {
     const response = await fetch(BASE_URL);
     data = await response.json();
     const imageURL = data.sprites.front_default;
+
+
 
     const imageDiv = document.getElementsByClassName("imageBackground")[0];
     const imageHTML = `
@@ -37,8 +59,8 @@ async function fetchData() {
     
     const statsHTML =
     `<ul class = "statsList">
-    <li> height: ${height1}</li>
-    <li> weight: ${weight1}</li>
+    <li> height: ${height1}m</li>
+    <li> weight: ${weight1}kg</li>
     <li> hp: ${hp1}</li>
     <li> attack: ${attack1}</li>
     <li> defense: ${defense1}</li>
@@ -48,8 +70,25 @@ async function fetchData() {
     </ul>
     `
     statsDiv.innerHTML = statsHTML;
-    
 
+    const typeMap = data.types.map(typeData => typeData.type.name);
+    var typeHTML = `<ul class = "typeList">`;
+    let valC = "";
+
+    for (var i = 0; i < typeMap.length; i++) {
+        valC = typeColor[typeMap[i]];
+        typeHTML += `\n<li style="background-color: #${c22e28}; 
+        width: 80px; height: 25px; border-radius: 5px; font-family: Inter, sans-serif;
+        text-align: center; line-height: 25px"> ${typeMap[i]} </li>`
+    }
+    
+    typeHTML += `\n <ul>`;
+    
+    document.getElementsByClassName("typesHeader")[0].innerHTML = typeHTML;
+
+    const indexNumber = document.getElementsByClassName("index")[0];
+    const indexHTML = `<p> ${valC} </p>`;
+    indexNumber.innerHTML = indexHTML;
 
 }
 
@@ -57,6 +96,10 @@ const infoButton = document.getElementsByClassName("Info")[0];
 const movesButton = document.getElementsByClassName("Moves")[0];
 
 infoButton.addEventListener("click", () => {
+
+    const infoText = document.getElementsByClassName("infoText")[0];
+    infoText.textContent = "Info";
+
     const height1 = data.height/10;
     const weight1 = data.weight/10;
     const hp1 = data.stats[0].base_stat/10;
@@ -71,8 +114,8 @@ infoButton.addEventListener("click", () => {
     
     const statsHTML =
     `<ul class = "statsList">
-    <li> height: ${height1}</li>
-    <li> weight: ${weight1}</li>
+    <li> height: ${height1}m</li>
+    <li> weight: ${weight1}kg</li>
     <li> hp: ${hp1}</li>
     <li> attack: ${attack1}</li>
     <li> defense: ${defense1}</li>
@@ -85,13 +128,22 @@ infoButton.addEventListener("click", () => {
 });
 
 movesButton.addEventListener("click", () => {
-    const moves = data.moves.map(moveData => moveData.move.name);
 
+    const infoText = document.getElementsByClassName("infoText")[0];
+    infoText.textContent = "Moves";
+
+    const moves = data.moves.map(moveData => moveData.move.name);
     const statsDiv = document.getElementsByClassName("statsList1")[0];
     
     var statsHTML = `<ul class = "statsList">`; 
+
+    let length = moves.length
+
+    if (length >= 13) {
+        length = 13;
+    }
     
-    for (var i = 0; i < moves.length; i++) {
+    for (var i = 0; i < length; i++) {
         console.log(moves[i]);
         statsHTML += `\n<li> ${moves[i]} </li>`;
     }
@@ -111,12 +163,17 @@ leftButton.addEventListener("click", () => {
         warningDiv.innerHTML = warningHTML;
     } else {
         index-=1;
+        BASE_URL = `https://pokeapi.co/api/v2/pokemon/${index}/`;
         fetchData();
     }
 });
 
 rightButton.addEventListener("click", () => {
-    index+=1;
+    index += 1;
+    BASE_URL = `https://pokeapi.co/api/v2/pokemon/${index}/`;
+    if (document.getElementsByClassName("warning0")[0] != null) {
+        document.getElementsByClassName("warning0")[0].remove();
+    }
     fetchData();
 });
 
